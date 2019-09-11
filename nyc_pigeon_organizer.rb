13b9785596f25a3000d6ddb04d25f67e6678fa84
Peter Werner <pcw1995@yahoo.com>
2019-09-11 22:56:16 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash= {}
+  data.each_key do |key1|
+    data[key1].each_key do |key2|
+      i = 0
+      while data[key1][key2].length>i  do 
+        if (!hash.key?(data[key1][key2][i]) && key1 = :color) || data[key1][key2].count(data[key1][key2][i])>1 
+          hash[data[key1][key2][i]] = {
+            :color => ["#{key2}"]
+          }
+        end 
+        i +=1 
+      end 
+    end 
+  end 
+  p hash 
 end
