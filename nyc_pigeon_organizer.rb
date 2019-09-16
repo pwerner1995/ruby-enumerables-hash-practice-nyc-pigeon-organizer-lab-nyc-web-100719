911a02aeab882c5c8b6fcd8f274f57f1be587adf
Peter Werner <pcw1995@yahoo.com>
2019-09-16 17:03:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,9 +18,7 @@ def nyc_pigeon_organizer(data)
         elsif hash[name].key?(key1) 
           hash[name][key1]= hash[name][key1].push("#{key2}")
         else 
-          hash[name]= {
-            key1 => hash[name][key1].push("#{key2}")
-          }
+          hash[name][key1] = ["#{key2}"]
         end 
         i +=1
         array_key2 =[] 
