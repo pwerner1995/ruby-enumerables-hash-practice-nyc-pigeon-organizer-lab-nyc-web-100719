3bd18e943fe89bf996eec216d7e5a5ffbae5e978
Peter Werner <pcw1995@yahoo.com>
2019-09-16 17:06:00 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,26 +1,22 @@
 def nyc_pigeon_organizer(data)
   hash= {}
-  array_key2= []
-  name = ""
   data.each_key do |key1|
-    
     data[key1].each_key do |key2|
       i = 0
       while data[key1][key2].length>i  do 
         name = data[key1][key2][i]
         if !hash.key?(name)
-          array_key2=[]
           hash[name]= {
-            key1 => array_key2.push("#{key2}")
+            key1 => ["#{key2}"]
           }
-        elsif !array_key2.include?("#{key2}") 
-          hash[name][key1]= array_key2.push("#{key2}")
+        elsif hash[name].key?(key1) 
+          hash[name][key1]= hash[name][key1].push("#{key2}")
+        else 
+          hash[name][key1] = ["#{key2}"]
         end 
         i +=1
-        array_key2=[]
-        puts array_key2
       end 
     end 
-  end 
+  end
   p hash 
 end
